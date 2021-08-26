0c6cb236f40e9c19020be9d156007f4df5c420fa
mustafa786ataul <ataulm786@gmail.com>
2021-08-26 15:17:47 +0530

    Routes

--- Blog/routes.py
+++ Blog/routes.py
@@ -34,7 +34,7 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             flash(f'You are logged in {user.username}', 'success')
