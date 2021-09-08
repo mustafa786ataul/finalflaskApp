23f13b0bd01196625794500fd8e908c3c3fbe1d0
ATAUL MUSTAFA <30733121+mustafa786ataul@users.noreply.github.com>
2021-09-08 15:32:09 +0530

    update db

--- Blog/__init__.py
+++ Blog/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Admin1234@database-1.c09apm6k4xjc.us-east-1.rds.amazonaws.com:5432/manage-user'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Admin1234@aa5sb5h1qmvgfh.cxy6d3sioze1.us-east-1.rds.amazonaws.com:5432/manage-user'
 
 
 db = SQLAlchemy(app)
